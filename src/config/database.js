a732a828b6769d3f0d7d6a0567219a7556b6f50e
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-12-13 23:10:47 -0400

    config database

--- src/config/database.js
+++ src/config/database.js
@@ -2,16 +2,22 @@ require('dotenv').config({
   path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
 });
 module.exports = {
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    dialect: 'postgres',
-    operatorAliases: false,
-    logging: false,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: 'postgres',
+  operatorAliases: false,
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  };
+  },
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};

ccbcbf68d959bf406168ec17cf7a31ab5bc18de7
JVictor <joaovictorb.dasilva2003@gmail.com>
2021-12-17 01:08:15 -0400

    ajustes

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ const permissions = require("../src/app/services/auth_service");
 const authMiddleware = require("../src/app/middlewares/auth_middleware");
 routes.use('/login', AuthenticateUserController.login);
 routes.use('/register', RegisterUserController.register);
-routes.use(authMiddleware);
+//routes.use(authMiddleware);
 routes.use('/refresh-token', RefreshTokenController.refreshToken);
 routes.use('/forgot-password', ResetPasswordUserController.forgotPassword);
 routes.use('/reset-password', ResetPasswordUserController.resetPassword);

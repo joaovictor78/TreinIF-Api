36c0e5a1855f78c8c0cc84c003f995e5a6b23cba
JVictor <joaovictorb.dasilva2003@gmail.com>
2022-01-15 15:13:13 -0400

    ajustes

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -1,7 +1,6 @@
 const route = require("express").Router();
 const CoursesController = require("../app/modules/courses/CoursesController");
 const permissions = require("../app/middlewares/auth_service"); 
-route.post('/course', CoursesController.createCourse);
-route.use(permissions.allUsers);
+route.post('/course', permissions.isAdmin,  CoursesController.createCourse);
 route.get('/courses', permissions.allUsers, CoursesController.getAllCourses);
 module.exports = route;

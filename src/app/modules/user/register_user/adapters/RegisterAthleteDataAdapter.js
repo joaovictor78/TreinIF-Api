af0d9263828fbb16ee6071c90a63961ce46a435d
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-02-11 08:53:00 -0300

    ajustes

--- src/app/modules/user/register_user/adapters/RegisterAthleteDataAdapter.js
+++ src/app/modules/user/register_user/adapters/RegisterAthleteDataAdapter.js
@@ -5,8 +5,8 @@ class RegisterAthleteDataAdapter {
         try {
             console.log("Register Athlete Data");
             console.log(data);
-            const { blood_type, birth_date, CPF, RG, course_id } = await AthleteData.create(data);
-            return { blood_type, birth_date, CPF, RG, course_id };
+            const { id:athlete_id, blood_type, birth_date, CPF, RG, course_id } = await AthleteData.create(data);
+            return { athlete_id, blood_type, birth_date, CPF, RG, course_id };
         } catch (e) {
             throw e;
         }

63eaf560b37f0429ba703cbb15a2c1ab41534959
João Victor <joaovictorb.dasilva2003@gmail.com>
2022-02-11 05:13:46 -0300

    ajustes

--- src/app/modules/user/authenticate_user/adapters/AuthenticateUserAdapter.js
+++ src/app/modules/user/authenticate_user/adapters/AuthenticateUserAdapter.js
@@ -8,8 +8,8 @@ class AuthenticateUserAdapter{
             const {id, full_name, avatar_url, role_id, password_hash} = await User.findOne( {where: {email} });
             console.log("adsfasdf", role_id)
             if(role_id == RolesEnum.Athlete){
-                const { birth_date, blood_type, CPF, RG, course_id } = await AthleteData.findOne({where: {user_id: id}});
-                return {id, full_name, email, avatar_url, role_id, password_hash, birth_date, blood_type, CPF, RG, course_id }
+                const { id:athlete_id, birth_date, blood_type, CPF, RG, course_id } = await AthleteData.findOne({where: {user_id: id}});
+                return {id, athlete_id, full_name, email, avatar_url, role_id, password_hash, birth_date, blood_type, CPF, RG, course_id }
             }
             return {id, full_name, email, avatar_url, role_id, password_hash};
         } catch(error){
